875b1d988ce98effaae3518436c7c1f1b5f2ec84
17BudgeS <17BudgeS@fulford.york.sch.uk>
2022-09-09 15:04:39 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many pizzas left");
+            int answer = int.Parse(Console.ReadLine());
+
+            for (int i = 0;)
+            {
+
+            }
+            //cooment 1
+            //coment2
+            //comment3
+        }
+    }
+}
